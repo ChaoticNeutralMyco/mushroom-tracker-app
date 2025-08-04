313589d844b20620c6854fafc291fa4f2809cb49
ChaoticNeutralMyco <vmoney91@gmail.com>
2025-08-03 19:25:22 -0600

    Clean up postcss.config.js and remove merge markers

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-module.exports = {
-=======
+// postcss.config.js
 export default {
->>>>>>> be7d1a18 (Initial commit with final polished version)
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-<<<<<<< HEAD
-}
-=======
 };
->>>>>>> be7d1a18 (Initial commit with final polished version)
